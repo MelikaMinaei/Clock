0f5a04abfbddab16882f7643dddc9a9726902b4f
Melika Minaei <105879977+MelikaMinaei@users.noreply.github.com>
2022-06-19 13:29:46 -0400

    Add files via upload

---
+++ MTLClock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+
+public class MTLClock : MonoBehaviour
+{
+    [SerializeField] Transform hourArm, minuteArm, secondArm;
+    const float hoursDegree = -30f, minutesDegree = -6f, secondsDegree = -6f;
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hourArm.localRotation = Quaternion.Euler(0f, 0f, hoursDegree * (float)time.TotalHours);
+        minuteArm.localRotation = Quaternion.Euler(0f, 0f, minutesDegree * (float)time.TotalMinutes);
+        secondArm.localRotation = Quaternion.Euler(0f, 0f, secondsDegree * (float)time.TotalSeconds);
+    }
+}
---
+++ TehranClock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+
+public class TehranClock : MonoBehaviour
+{
+    [SerializeField] Transform hourArm, minuteArm, secondArm;
+    const float hoursDegree = -30f, minutesDegree = -6f, secondsDegree = -6f;
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hourArm.localRotation = Quaternion.Euler(0f, 0f, hoursDegree * (((float)time.TotalHours) + 8));
+        minuteArm.localRotation = Quaternion.Euler(0f, 0f, minutesDegree * (((float)time.TotalMinutes) + 30));
+        secondArm.localRotation = Quaternion.Euler(0f, 0f, secondsDegree * (float)time.TotalSeconds);
+    }
+}
